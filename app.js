ff52e2e3a5c796ab7e3e06aa68f6d117b473d751
Morning <morning@1-10.local>
2023-03-20 12:42:35 +0800

    cloudinary

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
 import express from "express";
-// import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 import db from "./config/mongooseconfig.js";
-import Movie from "./routes/products.js";
+import movie from "./model/movie.js";
+import cloudinary from "./config/cloudinary.js";
 
 const port = 2200;
 const app = express();
@@ -15,7 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 db.once("open", () => {
-  Movie.find({})
+  movie
+    .find({})
     .limit(1)
     .then((movies) => {
       console.log("Movies", movies);
@@ -25,6 +22,25 @@ db.once("open", () => {
 db.on("error", (error) => {
   console.log("error", error);
 });
+
+//cloudinary upload
+const res = cloudinary.v2.uploader.upload(
+  "/Users/morning/Downloads/download.jpeg",
+  {
+    // public_id: "olympic_flag",
+    folder: "cat",
+    use_filename: true,
+  }
+);
+
+res
+  .then((data) => {
+    console.log(data);
+    console.log(data.secure_url);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 // const mongo_connection =
 //   "mongodb+srv://nominest:YgJhyP7RQCnJbyjs@cluster0.puiwl6z.mongodb.net/?retryWrites=true&w=majority";
 // mongoose
---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dfwhxsxqb",
+  api_key: "673179934243985",
+  api_secret: "a3lAQFUe889P7_s4rKIzIJSH6xI",
+});
+
+export default cloudinary;
